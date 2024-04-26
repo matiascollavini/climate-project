86cae3b79a26c0854eb0d0285fab1fb6e674a831
matias collavini <mncollavini@gmail.com>
2024-04-25 21:49:29 -0300

    Upload changes to github

---
+++ src/app/_ui/sunny-with-clouds.tsx
@@ -0,0 +1,16 @@
+export default function SunnyWithClouds () {
+  return(
+    <div className="container">
+      <div className="cloud front">
+        <span className="left-front"></span>
+        <span className="right-front"></span>
+      </div>
+      <span className="sun sunshine"></span>
+      <span className="sun"></span>
+      <div className="cloud back">
+        <span className="left-back"></span>
+        <span className="right-back"></span>
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
 import { fetchWeatherData } from "./_actions";
+import SunnyWithClouds from "./_ui/sunny-with-clouds";
 
 export default async function Home() {
       const climateApi = await fetchWeatherData()
   return (
   <main className="flex flex-col justify-center items-center h-screen gap-10">
-    <div className="flex flex-col justify-center items-center gap-2">
-      <h1 className="text-xl">Grados Celsius</h1>
-      <div>
-        <p>Temperatura: {climateApi.temp_c}°</p>
-        <p>Sensacion termica: {climateApi.feelslike_c}°</p>
+    <div className="flex flex-row justify-center items-center border rounded-lg bg-gray-200 shadow p-5">
+      <div className="flex justify-start items-center">
+        <SunnyWithClouds />
       </div>
-    </div>
-    <div className="flex flex-col justify-center items-center gap-2">
-      <h1 className="text-xl">Grados Fahrenheit</h1>
-      <div>
-        <p>Temperatura: {climateApi.temp_f}°</p>
-        <p>Sensacion termica: {climateApi.feelslike_f}°</p>
+      <div className="flex justify-end items-center">
+        <div className="flex flex-col justify-center items-start gap-2">
+            <h1 className="text-2xl">°C</h1>
+          <div>
+            <p>Temperatura: {climateApi.temp_c}°</p>
+            <p>Sensacion termica: {climateApi.feelslike_c}°</p>
+          </div>
+        </div>
       </div>
     </div>
   </main>
