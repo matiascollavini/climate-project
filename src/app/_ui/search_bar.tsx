8256fa1687a0b0bbb660bdd9d3c9a70b8ad2c496
imaagus <imaaugus04@gmail.com>
2024-04-26 16:50:40 -0300

    search bar operation

--- src/app/_ui/search_bar.tsx
+++ src/app/_ui/search_bar.tsx
@@ -1,20 +1,35 @@
 'use client'
-
-import { RiSearchLine } from '@remixicon/react';
-import { TextInput } from '@tremor/react';
 import { FaSearchLocation } from 'react-icons/fa';
-import { locations } from '../_data/lDate';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function SearchBar() {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
 
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams)
+    params.set('page', '1')
+      if (term) {
+      params.set('query', term)
+    }else{
+      params.delete('query')
+    }
+    replace(`${pathname}?${params.toString()}`)
+    }, 300)
+  
   return (
-    <form className='w-[500px] relative'>
       <div className='relative'>
-        <input type="search" placeholder='Buscar clima por localidad' className='w-full p-4 rounded-full bg-slate-800 text-white' />
+        <input type="search" placeholder='Buscar clima por localidad' className='w-full p-4 rounded-full bg-slate-800 text-white' 
+          defaultValue={searchParams.get('query')?.toString()}
+          onChange={(e) => {
+            handleSearch(e.target.value)
+          }} 
+        />
         <button className='absolute right-1 top-1/2 -translate-y-1/2 p-4 bg-slate-900 rounded-full text-white'>
           <FaSearchLocation />
         </button>
       </div>
-    </form>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,47 @@
 import { fetchWeatherData } from "./_actions";
+import { locations } from "./_consts/locations";
 import ClimateCard from "./_components/cards";
 import SunnyWithClouds from "./_ui/sunny-with-clouds";
 import SearchBar from "./_ui/search_bar";
 
-export default async function Home() {
-  
-   const locations = [
-    { id: 1, location: 'Banfield', cords: '-34.74,-58.40' },
-    { id: 2, location: 'Lomas de Zamora', cords: '-34.76,-58.42' },
-    { id: 3, location: 'Remedios de Escalada', cords: '-34.72,-58.40' },
-    { id: 4, location: 'Lanús', cords: '-34.69,-58.41' },
-    { id: 5, location: 'Avellaneda', cords: '-34.66,-58.36' }
-  ]
+interface Location {
+  id: number;
+  location: string;
+  cords: string;
+}
+
+interface SearchParams {
+  query?: string;
+  page?: string;
+}
+
+export default async function Home({ searchParams }: { searchParams?: SearchParams }) {
+  const query = searchParams?.query || '';
+
+ // Función para normalizar una cadena eliminando los acentos
+ const normalizeString = (str: string) => {
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+};
+
+// Filtrar las ubicaciones basadas en la consulta de búsqueda normalizada
+const filteredLocations: Location[] = locations.filter((location: Location) =>
+  normalizeString(location.location.toLowerCase()).includes(normalizeString(query.toLowerCase()))
+);
+
   return (
     <>
       <header></header>
       <main className="container min-h-[calc(100vh-121px)] mx-auto pb-10 px-6 flex justify-center items-center h-screen gap-10">
         <div className="grid grid-flow-col grid-rows-2 justify-center items-center gap-4">
-        <SearchBar />
-        {locations.map((location: any) => 
-          <div key={location.id} className="min-h-[200px] min-w-[500px]">
-            <ClimateCard locationID={location.cords} locationName={location.location} />
-          </div>
-        )}
+          <SearchBar />
+          {filteredLocations.map((location: Location) =>
+            <div key={location.id} className="min-h-[200px] min-w-[500px]">
+              <ClimateCard locationID={location.cords} locationName={location.location} />
+            </div>
+          )}
         </div>
       </main>
       <footer></footer>
     </>
-  )
+  );
 }
