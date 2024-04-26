0a30ece5a6acf9ba210e9dcbbc7044ee7a6a0a2f
matias collavini <mncollavini@gmail.com>
2024-04-26 00:28:33 -0300

    MAIN - Push to github

--- src/app/_components/cards.tsx
+++ src/app/_components/cards.tsx
@@ -4,17 +4,16 @@ import SunnyWithClouds from "../_ui/sunny-with-clouds";
 export default async function ClimateCard ({ locationID, locationName }: { locationID: string, locationName: string }) {
   const climateApi = await fetchWeatherData('current', locationID)
   return (
-    <div className="flex flex-row justify-center items-center border rounded-lg bg-gray-200 shadow p-5">
+    <div className="flex flex-row justify-center items-center rounded-lg bg-gradient-to-b from-[#353d49] to-[#2f2c2c] shadow p-5">
       <div className="flex justify-start items-center">
-        <SunnyWithClouds />
+        <SunnyWithClouds climate={climateApi.wx_desc} />
       </div>
-      <div className="flex flex-col justify-end items-start w-full">
-        <h1 className="text-2xl">{locationName}</h1>
+      <div className="flex flex-col justify-end items-start w-full text-white">
+        <h1 className="text-2xl font-medium">{locationName}</h1>
         <div className="flex flex-col justify-center items-start gap-2">
-            <h1 className="text-2xl">{climateApi.temp_c}°C</h1>
+            <h1 className="text-2xl font-bold">{climateApi.temp_c}°C</h1>
           <div>
-            <p>Temperatura: {climateApi.temp_c}°</p>
-            <p>Sensacion termica: {climateApi.feelslike_c}°</p>
+            <p>Sensacion termica: {climateApi.feelslike_c}°C</p>
           </div>
         </div>
       </div>
--- src/app/_ui/sunny-with-clouds.tsx
+++ src/app/_ui/sunny-with-clouds.tsx
@@ -1,16 +1,29 @@
-export default function SunnyWithClouds () {
+export default function SunnyWithClouds ({ climate }: any) {
   return(
     <div className="container">
+      {(climate === 'Partly cloudy' || climate === 'Cloudy') &&
       <div className="cloud front">
         <span className="left-front"></span>
         <span className="right-front"></span>
       </div>
-      <span className="sun sunshine"></span>
-      <span className="sun"></span>
+      }
+      {(climate === 'Sunny' || climate === 'Partly cloudy') &&
+        <>
+          <span className="sun sunshine"></span>
+          <span className="sun"></span>
+        </>
+      }
+      {(climate === 'Partly cloudy' || climate === 'Cloudy') &&
       <div className="cloud back">
         <span className="left-back"></span>
         <span className="right-back"></span>
       </div>
+      }
+      {climate === 'Light Rain' &&
+      <div className="text-white">
+        (Light Rain)
+      </div>
+      }
     </div>
   )
 }
