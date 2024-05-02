6a8566bc25c134af45c14ece50b74b7b5bfce7b9
matias collavini <mncollavini@gmail.com>
2024-05-02 01:02:08 -0300

    Fix some colors in modal dialogs

--- src/app/_ui/dialog-modal.tsx
+++ src/app/_ui/dialog-modal.tsx
@@ -9,7 +9,7 @@ export default function DialogModal ({ showDialog, setShowDialog, size, title, c
       static={true}
       className="z-[100]"
     >
-      <DialogPanel className={`flex flex-col justify-start py-4 px-4 dark:bg-[#111827] ${size ? size : 'max-w-[600px]'}`}>
+      <DialogPanel className={`flex flex-col justify-start py-4 px-4 bg-gradient-to-b from-slate-300 to-slate-400 dark:from-[#353d49] dark:to-[#2f2c2c] ${size ? size : 'max-w-[600px]'}`}>
         <div className='py-5 flex items-center justify-between'>
           <div>
             <GlobeAmericasIcon className="w-8 h-8 text-black dark:text-white" />
